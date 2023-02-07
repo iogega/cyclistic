e1d532cc7482c55e895723734802b84b095a925c
iogega <105914112+iogega@users.noreply.github.com>
2023-02-07 15:58:16 +0300

    Update seasonal_total_rides_query.sql

--- seasonal_total_rides_query.sql
+++ seasonal_total_rides_query.sql
@@ -17,7 +17,7 @@ FROM `cyclistic-344413.trip_data.combined_data`
 WHERE started_at BETWEEN '2021-06-01 00:00:00' AND '2021-08-31 23:59:59'
 GROUP BY member_casual
 
--- Autum total rides by type of rider
+-- Autumn total rides by type of rider
 SELECT member_casual, COUNT(DISTINCT ride_id) AS total_rides
 FROM `cyclistic-344413.trip_data.combined_data`
 WHERE started_at BETWEEN '2021-09-01 00:00:00' AND '2021-11-30 23:59:59'
